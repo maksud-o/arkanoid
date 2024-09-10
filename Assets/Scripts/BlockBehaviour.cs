70e193e3e8ca3b365b3c89124ea8f67fc1f38e6a
maksud-work <privetyaarseniy@gmail.com>
2024-09-10 15:51:17 +0300

    multi hit block implementation + prefab

--- Assets/Scripts/BlockBehaviour.cs
+++ Assets/Scripts/BlockBehaviour.cs
@@ -1,15 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
 public class BlockBehaviour : MonoBehaviour
 {
     [SerializeField] private string _ballTag;
+    [Tooltip("Can be null")][SerializeField] private List<Sprite> _multipleHitsSprites;
+
+    private SpriteRenderer _spriteRenderer;
+    private bool _isMultiHit = true;
+    private int _hitStage = 0;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (_multipleHitsSprites?.Count == 0)
+        {
+            _isMultiHit = false;
+        }
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag(_ballTag))
+        if (collision.gameObject.CompareTag(_ballTag))
         {
-            gameObject.SetActive(false);
+            if (!_isMultiHit || !IsAlive())
+            {
+                gameObject.SetActive(false);
+            }
         }
+    }
+
+    private bool IsAlive()
+    {
+        if (_hitStage < _multipleHitsSprites.Count)
+        {
+            _spriteRenderer.sprite = _multipleHitsSprites[_hitStage];
+        }
+        else
+        {
+            return false;
+        }
+        _hitStage++;
+        return true;
     }
 }
