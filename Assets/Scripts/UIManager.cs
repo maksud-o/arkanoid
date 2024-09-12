72c1ff86bbd258ac14457f624920afddc8cc7544
maksud-work <privetyaarseniy@gmail.com>
2024-09-12 09:59:55 +0300

    true string template

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,17 +1,18 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     [Header("Score Settings")]
-    [SerializeField] private string _scoreTemplate = "Score: ";
+    [SerializeField] private string _scoreTemplate = "Score: {0}";
     [SerializeField] private TextMeshProUGUI _scoreText;
 
     private int _currentScore = 0;
 
     private void Awake()
     {
-        _scoreText.text = $"{_scoreTemplate}{_currentScore}";
+        ChangeScoreText(_scoreTemplate, _currentScore);
     }
 
     private void OnEnable()
@@ -27,6 +28,11 @@ public class UIManager : MonoBehaviour
     private void ChangeScore(int score)
     {
         _currentScore += score;
-        _scoreText.text = $"{_scoreTemplate}{_currentScore}";
+        ChangeScoreText(_scoreTemplate, _currentScore);
+    }
+
+    private void ChangeScoreText(string template, int score)
+    {
+        _scoreText.text = string.Format(_scoreTemplate, score);
     }
 }
