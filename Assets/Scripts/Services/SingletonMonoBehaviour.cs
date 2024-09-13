10b52f3dd1b0f035d1a8f789504be292c38c49ca
maksud-work <privetyaarseniy@gmail.com>
2024-09-13 16:58:24 +0300

    singleton service abstract

---
+++ Assets/Scripts/Services/SingletonMonoBehaviour.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Arkanoid.Services
+{
+    public abstract class SingletonMonoBehaviour<T> : MonoBehaviour
+    {
+        #region Properties
+
+        public static T Instance { get; private set; }
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            transform.SetParent(null);
+            DontDestroyOnLoad(gameObject);
+            Instance = GetComponent<T>();
+        }
+
+        #endregion
+    }
+}
