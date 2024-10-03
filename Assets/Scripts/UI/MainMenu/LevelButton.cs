8bbb995a98477ae5c21293d8bb0143167cc46ce7
maksud-o <privetyaarseniy@gmail.com>
2024-10-03 21:56:17 +0300

    add level buttons

---
+++ Assets/Scripts/UI/MainMenu/LevelButton.cs
@@ -0,0 +1,46 @@
+﻿using Arkanoid.Services;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Arkanoid.UI.MainMenu
+{
+    [RequireComponent(typeof(Button))]
+    public class LevelButton : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private TextMeshProUGUI _buttonText;
+        private string _levelSceneName;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(LoadLevel);
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Setup(string levelSceneName, int levelIndex)
+        {
+            _buttonText.text = $"{levelIndex}";
+            _levelSceneName = levelSceneName;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void LoadLevel()
+        {
+            ScenesService.Instance.LoadScene(_levelSceneName);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/LevelButtonsSetup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Arkanoid.UI.MainMenu
+{
+    public class LevelButtonsSetup : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private string[] _levelScenesNames;
+        [SerializeField] private LevelButton _levelButtonPrefab;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            var index = 1;
+            foreach (string levelScenesName in _levelScenesNames)
+            {
+                Instantiate(_levelButtonPrefab.gameObject, transform).GetComponent<LevelButton>()
+                    .Setup(levelScenesName, index++);
+            }
+        }
+
+        #endregion
+    }
+}
